df7294dcb8b8e6a930cfdffb043d3db5849dc320
katyaynishukla <81090089+katyaynishukla@users.noreply.github.com>
2024-04-20 19:10:35 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 exports.connect = () => {
 	mongoose
 		.connect("mongodb://127.0.0.1:27017/auth", {
-			// .connect("mongodb://localhost/auth_demo", {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		})
